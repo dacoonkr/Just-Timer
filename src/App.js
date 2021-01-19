3ce98ef53cec78846ba2bff45850b77196f889af
dacoonkr <dacoon0621@gmail.com>
2021-01-19 10:50:49 +0900

    타이틀

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import { Timer, Trainer, Algorithms, NotFound } from "./pages";
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-          <Switch>
-            <Route path="/" exact={true} component={Timer}/>
-            <Route path="/trainer" exact={true} component={Trainer}/>
-            <Route path="/algorithms" exact={true} component={Algorithms}/>
-            <Route component={NotFound} />
-          </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Bar></Bar>
+        <BrowserRouter>
+            <Switch>
+              <Route path="/" exact={true} component={Timer}/>
+              <Route path="/trainer" exact={true} component={Trainer}/>
+              <Route path="/algorithms" exact={true} component={Algorithms}/>
+              <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+class Bar extends Component {
+  render() {
+    return (
+      <>
+        <span className="h1">JUST TIMER</span>
+      </>
+    )
+  }
 }
 
 export default App;
