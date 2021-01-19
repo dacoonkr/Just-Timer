d3f8af00dfcdced2f21c0e4cd3ec06c745ced913
dacoonkr <dacoon0621@gmail.com>
2021-01-19 09:37:15 +0900

    페이지

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Timer, Trainer, Algorithms, NotFound } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      
-    
+      <BrowserRouter>
+          <Switch>
+            <Route path="/" exact={true} component={Timer}/>
+            <Route path="/trainer" exact={true} component={Trainer}/>
+            <Route path="/algorithms" exact={true} component={Algorithms}/>
+            <Route component={NotFound} />
+          </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Timer } from "./Timer";
+export { default as Trainer } from "./Trainer";
+export { default as Algorithms } from "./Algorithms.js";
+export { default as NotFound } from "./NotFound";
