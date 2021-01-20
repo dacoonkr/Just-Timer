1b049af692da5efeaf4a85d6317782e89f173408
dacoonkr <dacoon0621@gmail.com>
2021-01-20 13:24:58 +0900

    타이머 분리

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -1,10 +1,54 @@
 import React from "react";
+import { Component } from 'react';
 
 const Page = () => {
-    return (
-        <>
-        </>
-    );
+  return (
+    <>
+        <Scramble></Scramble>
+        <Content></Content>
+    </>
+  );
 };
+
+class Scramble extends Component {
+  render() {
+    return (
+      <>
+        L R2 B2 U2 F2 D' B2 L2 U F2 R2 F2 D' B' R' D B F' L2 D2 B2
+      </>
+    )
+  }
+}
+
+class History extends Component {
+  render() {
+    return (
+      <>
+        대충 table
+      </>
+    )
+  }
+}
+
+class Timer extends Component {
+  render() {
+    return (
+      <div className="Timer">
+        OO.
+        <span>OO</span>
+      </div>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        <Timer></Timer>
+      </div>
+    )
+  }
+}
 
 export default Page;
